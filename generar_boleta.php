3dc641f3c0d67dc09cd8d81c086657757357c65d
IGNRV <lgi.rodriguezvillarroel@gmail.com>
2024-01-05 16:30:39 -0300

    Avance con boleta

--- generar_boleta.php
+++ generar_boleta.php
@@ -15,7 +15,19 @@ $pdf->Cell(40, 10, 'Boleta de Venta');
 $pdf->Ln(20);
 
 // Listar los productos vendidos
+$pdf->SetFont('Arial', 'B', 12);
+
+// Encabezados de las columnas
+$pdf->Cell(40, 10, 'Nombre', 1);
+$pdf->Cell(40, 10, 'Precio Unitario', 1);
+$pdf->Cell(40, 10, 'Cantidad', 1);
+$pdf->Cell(40, 10, 'Total', 1);
+$pdf->Ln();
+
+// Restablecer la fuente para el contenido
 $pdf->SetFont('Arial', '', 12);
+
+// Listar los productos vendidos
 foreach ($productosVendidos as $producto) {
     $precio = (float)$producto['precio'];
     $cantidad = (int)$producto['cantidadVendida'];
