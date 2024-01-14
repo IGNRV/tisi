77f04e34742796f8f81f304dc3e90aeaf5de0ad7
Ubuntu <ubuntu@vps-b2b2bbac.vps.ovh.ca>
2024-01-14 23:49:53 +0000

    Avance

--- navbar.php
+++ navbar.php
@@ -1,9 +1,9 @@
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-white">
   <a class="navbar-brand" href="#">Mi Aplicación</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+  </button> -->
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav ml-auto">
       <!-- Reemplazar con ícono de cierre de sesión -->
