77b8ec9b8105168faa82fbc69b8a6f7e9b1cf8f9
Ubuntu <ubuntu@vps-b2b2bbac.vps.ovh.ca>
2024-01-19 15:32:19 +0000

    Avance

--- sidebar.php
+++ sidebar.php
@@ -4,25 +4,58 @@
 </div>
   <ul class="nav nav-pills flex-column mb-auto">
     <li class="nav-item">
-      <a href="welcome.php" class="nav-link text-black" style="color: black; font-size: 18px; margin-left: 18px">Venta</a>
+    <a href="welcome.php" class="nav-link text-black" style="color: black; font-size: 16px; margin-left: 18px">
+  <img src="https://trackitsellit.oralisisdataservice.cl/images/storefront_FILL0_wght400_GRAD0_opsz24.svg" class="icono-venta" alt="Ícono Venta">Venta
+</a>
     </li>
     <li class="nav-item">
-      <a href="welcome.php?page=products" class="nav-link text-black" style="color: black; font-size: 18px; margin-left: 18px">Productos</a>
+      <a href="welcome.php?page=products" class="nav-link text-black" style="color: black; font-size: 16px; margin-left: 18px">
+      <img src="https://trackitsellit.oralisisdataservice.cl/images/inventory_FILL0_wght400_GRAD0_opsz24.svg" class="icono-venta" alt="Ícono Productos">Productos
+      </a>
     </li>
     <li class="nav-item">
-      <a href="welcome.php?page=categorias" class="nav-link text-black" style="color: black; font-size: 18px; margin-left: 18px">Categorias</a>
+      <a href="welcome.php?page=categorias" class="nav-link text-black" style="color: black; font-size: 16px; margin-left: 18px">
+      <img src="https://trackitsellit.oralisisdataservice.cl/images/category_FILL0_wght400_GRAD0_opsz24.svg" class="icono-venta" alt="Ícono Categorias">Categorias
+      </a>
     </li>
     <li class="nav-item"> 
-      <a href="welcome.php?page=cuadratura" class="nav-link text-black" style="color: black; font-size: 18px; margin-left: 18px">Cuadratura de Caja</a>
+      <a href="welcome.php?page=cuadratura" class="nav-link text-black" style="color: black; font-size: 16px; margin-left: 18px">
+      <img src="https://trackitsellit.oralisisdataservice.cl/images/payments_FILL0_wght400_GRAD0_opsz24.svg" class="icono-venta" alt="Ícono Cuadratura de Cajas">Cuadratura de Caja
+      </a>
     </li>
-    <li class="nav-item"> 
-      <a href="welcome.php?page=proveedores" class="nav-link text-black" style="color: black; font-size: 18px; margin-left: 18px">Proveedores</a>
+    <li class="nav-item" style="padding-left: 18px;"> <!-- Añade el padding aquí -->
+      <a href="welcome.php?page=proveedores" class="nav-link text-black" style="color: black; font-size: 16px;">
+        <img src="https://trackitsellit.oralisisdataservice.cl/images/local_shipping_FILL0_wght400_GRAD0_opsz24.svg" class="icono-venta" alt="Ícono Proveedores">Proveedores
+      </a>
     </li>
     <li class="nav-item">
-      <a href="welcome.php?page=configuracion" class="nav-link text-black" style="color: black; font-size: 18px; margin-left: 18px">Configuracion</a>
+      <a href="welcome.php?page=configuracion" class="nav-link text-black" style="color: black; font-size: 16px; margin-left: 18px">
+        <img src="https://trackitsellit.oralisisdataservice.cl/images/manufacturing_FILL0_wght400_GRAD0_opsz24.svg" class="icono-venta" alt="Ícono Cuadratura de Cajas">Configuracion
+      </a>
     </li>
     <li class="nav-item">
-      <a href="welcome.php?page=suscripcion" class="nav-link text-black" style="color: black; font-size: 18px; margin-left: 18px">Suscripción</a>
+      <a href="welcome.php?page=suscripcion" class="nav-link text-black" style="color: black; font-size: 16px; margin-left: 18px">
+        <img src="https://trackitsellit.oralisisdataservice.cl/images/event_repeat_FILL0_wght400_GRAD0_opsz24.svg" class="icono-venta" alt="Ícono Cuadratura de Cajas">Suscripción
+      </a>
     </li>
   </ul>
 </div>
+<style>
+  /* Estilos existentes... */
+
+  .icono-venta {
+    height: 1em; /* Ajusta esto según sea necesario */
+    width: auto;
+    margin-right: 5px; /* Espacio entre el ícono y el texto */
+    vertical-align: middle; /* Alinea el ícono con el texto */
+  }
+  
+  /* Agregar estilo para la paginación */
+  .pagination a {
+    margin: 0 10px; /* Aumenta el margen a los lados del enlace */
+    text-decoration: none;
+    color: #4e73df;
+  }
+  
+  /* Otras reglas de estilo que ya tengas... */
+</style>
